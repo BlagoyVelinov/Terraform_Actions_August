811de6aac5ca16de24c66c89d1349ab47553a695
Blagoy <blagoy1234@gmail.com>
2025-08-01 15:40:10 +0300

    update terraform main file

--- main.tf
+++ main.tf
@@ -80,4 +80,5 @@ resource "azurerm_app_service_source_control" "asscontrol" {
   app_id   = azurerm_linux_web_app.alwapp.id
   repo_url = var.repo_URL
   branch   = "main"
+  use_manual_integration = true
 }
